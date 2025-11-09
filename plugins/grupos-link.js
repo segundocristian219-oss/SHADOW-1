a10b03722b549337c64a61155b19b42c2563d110
AngelBotsD <angelperez22448@gmail.com>
2025-11-09 13:42:56 -0600

    Actualizar grupos-link.js

--- plugins/grupos-link.js
+++ plugins/grupos-link.js
@@ -1,1 +1,11 @@
-// Archivo temporal para mantener la carpeta plugins
+async function handler(m, { conn }) {
+  let group = m.chat
+  let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+  await conn.reply(m.chat, link, m, { detectLink: true })
+}
+
+handler.command = ['link', 'enlace']
+handler.group = true
+handler.admin = true
+
+export default handler
