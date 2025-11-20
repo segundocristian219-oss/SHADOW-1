b0247612ed6b5d8d6bc16bb8f10a721b60c2ca77
AngelBotsD <angelperez22448@gmail.com>
2025-11-19 18:22:04 -0600

    Actualizar stickers-wm.js

--- plugins/stickers-wm.js
+++ plugins/stickers-wm.js
@@ -58,8 +58,6 @@ let handler = async (m, { conn, text }) => {
   )
 }
 
-handler.help = ['wm']
-handler.tags = ['sticker']
 handler.command = ['wm', 'take', 'robarsticker']
 
 export default handler
